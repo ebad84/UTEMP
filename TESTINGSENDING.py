ccda350348965463ec39dd515d5606dc58c0705d
Ebad84 <ebad84.025@gmail.com>
2026-01-01 01:06:15 +0330

    testing is done, TESTINGSENDING is working

--- TESTINGSENDING.py
+++ TESTINGSENDING.py
@@ -5,20 +5,21 @@ SENSOR_UUID = "5a1e4bed-9bc8-4c13-ae1a-a7caa26dcc93"
 API_KEY = "9210d017b80ee04a553fc681fded63868983b8fe7f0ec95dec51797523caa5a4"
 
 # مقدار دما برای آپلود
-temperature = 25.7
+temperature = 22.33
 
 # URL API (مثال لوکال)
 url = "http://127.0.0.1:5000/api/upload"
 
 # داده‌ها
-data = {
-    "uuid": SENSOR_UUID,
+headers = {
     "X-API-KEY": API_KEY,
+}
+data = {
     "temperature": temperature
 }
 
 # درخواست POST
-response = requests.post(url, json=data)
+response = requests.post(url, json=data, headers=headers)
 
 if response.status_code == 200:
     print("دما با موفقیت آپلود شد:", response.json())
